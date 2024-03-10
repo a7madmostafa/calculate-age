34dd7c945bb8b831e649a76d2d275cd9c70e486d
Ahmad Mostafa <a.m_eg@yahoo.com>
2024-03-10 11:29:06 +0200

    Update calc-age.py

--- calc-age.py
+++ calc-age.py
@@ -1,11 +1,11 @@
 import streamlit as st
+import time
 
-# st.title ("Calculate your age")
 st.image('https://collegevidya.com/tool/images/age.png', use_column_width=True)
 
 year = st.slider("Enter your birth year", 2000, 2023, 2000)
-current = st.slider("Enter the current year", 2024, 2050, 2024)
-
+# current = st.slider("Enter the current year", 2024, 2050, 2024)
+current = time.localtime().tm_year
 age = current - year
 
 if st.button("Calculate"):
